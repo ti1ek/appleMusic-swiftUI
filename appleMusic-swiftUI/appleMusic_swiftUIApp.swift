ecf3d007fe6d2590061e8668bf7fa5d82609e86b
ti1ek <tilek.koszhanov@mail.ru>
2023-07-02 19:25:09 +0500

    Initial Commit

---
+++ appleMusic-swiftUI/appleMusic_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleMusic_swiftUIApp.swift
+//  appleMusic-swiftUI
+//
+//  Created by ti1ek on 02.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct appleMusic_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
